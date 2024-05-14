25e0a021a07b545680c84a185c51314472c7b524
TheSaulMx <saaulmx@gmail.com>
2024-05-14 16:48:55 -0600

    new navigation config

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeTabs } from './navigation/HomeTabs';
-import { HomeHeader } from './navigation/HomeHeader';
-import { ParamListBase, RouteProp } from '@react-navigation/native';
-import { ChatTabs } from './navigation/ChatTabs';
-
-const Stack = createNativeStackNavigator();
-
-const screenOptions = ({ route }: { route: RouteProp<ParamListBase, string> }) => ({
-  header: ['Home'].includes(route.name)
-    ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      ({ options }: { options: any }) => {
-        // const title = getHeaderTitle(options, route.name);
-        return <HomeHeader />;
-      }
-    : undefined,
-});
+import Routes from './navigation';
+import { AuthProvider } from './contexts/userAuth';
 
 const App = () => {
   return (
-    <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="Home" component={HomeTabs} />
-      <Stack.Screen name="Chat" component={ChatTabs} options={{ headerShown: false }} />
-    </Stack.Navigator>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 };
 
