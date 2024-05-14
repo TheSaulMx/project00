d68bbceed16e817cabd790671e4acbb658177a76
TheSaulMx <saaulmx@gmail.com>
2024-05-14 16:53:13 -0600

    change the useAuth hook path

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
 import { Button } from 'react-native-paper';
-import { useAuth } from '../../contexts/userAuth';
+import { useAuth } from '../../contexts/auth';
 
 export default function Profile() {
   const { signOut } = useAuth();
