ecbdbe200a8842ea12a67122078ea59c58b72ddb
TheSaulMx <saaulmx@gmail.com>
2024-05-17 08:44:37 -0600

    Added the NavigationContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import Routes from './navigation';
+import Routes from './routes';
 import { AuthProvider } from './contexts/auth';
+import { NavigationContainer } from '@react-navigation/native';
+import { navigationRef } from './utils/rootNavigation';
 
 const App = () => {
   return (
     <AuthProvider>
-      <Routes />
+      <NavigationContainer ref={navigationRef}>
+        <Routes />
+      </NavigationContainer>
     </AuthProvider>
   );
 };
