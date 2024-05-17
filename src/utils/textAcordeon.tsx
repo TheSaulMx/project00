b9b443b4d9205e28ef9b71fd7144397d505e81c1
TheSaulMx <saaulmx@gmail.com>
2024-05-17 08:42:50 -0600

    removed unused imports

--- src/utils/textAcordeon.tsx
+++ src/utils/textAcordeon.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
