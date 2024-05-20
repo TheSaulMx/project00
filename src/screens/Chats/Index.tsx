194f25f703556a5cc02b58a42e9e36bffbde0b0b
TheSaulMx <saaulmx@gmail.com>
2024-05-20 09:10:45 -0600

    new method to handle the back button, onlyu in case the app was opened from quit state

--- src/screens/Chats/Index.tsx
+++ src/screens/Chats/Index.tsx
@@ -3,6 +3,10 @@ import { ChatTabsHeader } from '../../routes/ChatTabsHeader';
 import { ChatPrivateHeader } from '../../components/ChatPrivateHeader/ChatPrivateHeader';
 import Chats from './Chats';
 import { ChatPrivate } from './ChatPrivate';
+import { useEffect } from 'react';
+import { BackHandler } from 'react-native';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 const Stack = createStackNavigator();
 const screenOptions = ({ route }: { route: any }) => ({
@@ -14,10 +18,25 @@ const screenOptions = ({ route }: { route: any }) => ({
     }
   },
 });
-export function Chat() {
+export function Chat({ route }: { route: any }) {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const { fromQuitState } = route.params;
+  useEffect(() => {
+    function handlerAcion() {
+      if (fromQuitState) {
+        navigation.navigate('Main');
+      }
+      return true;
+    }
+    const unsubscribe = BackHandler.addEventListener('hardwareBackPress', handlerAcion);
+
+    return () => {
+      unsubscribe.remove();
+    };
+  }, [fromQuitState, navigation]);
   return (
     <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="Chat" component={Chats} />
+      <Stack.Screen name="Chats" component={Chats} />
       <Stack.Screen name="ChatPrivate" component={ChatPrivate} />
     </Stack.Navigator>
   );
