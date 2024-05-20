ce11984f9d0e5ed9be15eb46a8fbe68f347c2f49
TheSaulMx <saaulmx@gmail.com>
2024-05-20 09:08:23 -0600

    new file to render the main routes

---
+++ src/routes/main.routes.tsx
@@ -0,0 +1,39 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home/Home';
+import Profile from '../screens/Profile/Profile';
+import TabBarIcon from '../components/TabBarIcon/TabBarIcon';
+import { TabBarIconMappings } from '../utils/bottomTabBarIconMapping';
+import { ParamListBase, RouteProp } from '@react-navigation/native';
+import { ActivityIndicator } from 'react-native';
+import { useMessaging } from '../hooks/useMessaging';
+import { HomeHeader } from '../components/HomeHeader/HomeHeader';
+import { Chat } from '../screens/Chats/Index';
+
+const Tab = createBottomTabNavigator();
+const screenOptions = ({ route }: { route: RouteProp<ParamListBase, string> }) => ({
+  tabBarIcon: ({ focused }: { focused: boolean }) => {
+    const { focus, unfocused } = TabBarIconMappings[route.name] || {};
+    return <TabBarIcon focused={focused} iconSourceFocused={focus} iconSourceUnfocused={unfocused} />;
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  header: ({ options }: { options: any }) => {
+    return <HomeHeader />;
+  },
+});
+
+export function MainRoutes() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { initialRoute, loading, initialNotification, fromQuitState } = useMessaging();
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <Tab.Navigator screenOptions={screenOptions} initialRouteName={initialRoute}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Chat" component={Chat} />
+    </Tab.Navigator>
+  );
+}
