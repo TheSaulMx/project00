9164b5ac07c3d4031495fc6b2a47c731a9d77f66
TheSaulMx <saaulmx@gmail.com>
2024-05-20 09:10:05 -0600

    change in the navigator screens

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,44 +1,19 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../screens/Home/Home';
-import Profile from '../screens/Profile/Profile';
-import TabBarIcon from '../components/TabBarIcon/TabBarIcon';
-import { TabBarIconMappings } from '../utils/bottomTabBarIconMapping';
-import { ParamListBase, RouteProp } from '@react-navigation/native';
-import { ActivityIndicator } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MainRoutes } from './main.routes';
 import { useMessaging } from '../hooks/useMessaging';
-import { HomeHeader } from '../components/HomeHeader/HomeHeader';
-import { Chat } from '../screens/Chats/Index';
+import { ActivityIndicator } from 'react-native';
 
-const Tab = createBottomTabNavigator();
-const screenOptions = ({ route }: { route: RouteProp<ParamListBase, string> }) => ({
-  tabBarIcon: ({ focused }: { focused: boolean }) => {
-    const { focus, unfocused } = TabBarIconMappings[route.name] || {};
-    return <TabBarIcon focused={focused} iconSourceFocused={focus} iconSourceUnfocused={unfocused} />;
-  },
-  tabBarButton: ['Chat'].includes(route.name)
-    ? () => {
-        return null;
-      }
-    : undefined,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  header: ({ options }: { options: any }) => {
-    return <HomeHeader />;
-  },
-});
-
+const Stack = createStackNavigator();
 export function AppRoutes() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { initialRoute, loading, initialNotification } = useMessaging();
+  const { initialRoute, loading } = useMessaging();
 
   if (loading) {
     return <ActivityIndicator />;
   }
 
   return (
-    <Tab.Navigator screenOptions={screenOptions} initialRouteName={initialRoute}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Chat" component={Chat} options={{ headerShown: false }} />
-    </Tab.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={initialRoute}>
+      <Stack.Screen name="Main" component={MainRoutes} />
+    </Stack.Navigator>
   );
 }
