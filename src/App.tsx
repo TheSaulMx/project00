583556e9d91b8bb3cce5938231d37254957a275b
TheSaulMx <saaulmx@gmail.com>
2024-05-23 17:44:31 -0600

    new NavigationContainer with methods to update the currentScreenName

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,31 @@ import Routes from './routes';
 import { AuthProvider } from './contexts/auth';
 import { NavigationContainer } from '@react-navigation/native';
 import { navigationRef } from './utils/rootNavigation';
+import { useRef } from 'react';
 
 const App = () => {
+  const routeNameRef = useRef<string | undefined>();
+
   return (
     <AuthProvider>
-      <NavigationContainer ref={navigationRef}>
+      <NavigationContainer
+        ref={navigationRef}
+        onReady={() => {
+          routeNameRef.current = navigationRef.getCurrentRoute()?.name;
+        }}
+        onStateChange={async () => {
+          const previusRouteName = routeNameRef.current;
+          const currentRouteName = navigationRef.getCurrentRoute()?.name;
+          const trackScreenView = async (currentRoute: any) => {
+            console.log('current Route Name: ', currentRoute);
+          };
+          if (previusRouteName !== currentRouteName) {
+            // Save the current route name for later comparison
+            routeNameRef.current = currentRouteName;
+            // Replace the line below to add the tracker from a mobile analytics SDK
+            await trackScreenView(currentRouteName);
+          }
+        }}>
         <Routes />
       </NavigationContainer>
     </AuthProvider>
